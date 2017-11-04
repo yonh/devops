a24486b4fd96ed45237827d8a814f7c40fd476c0
Ubuntu <ubuntu@ip-172-31-30-181.ap-northeast-1.compute.internal>
2017-11-04 07:39:50 +0000

    init

---
+++ 1.run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+# sudo usermod -aG docker `whoami`
+
+
+
+
+
+# install gogs
+sudo docker pull gogs/gogs
+sudo docker pull jenkins/jenkins:2.72
+sudo docker pull maven:3.5.0-jdk-8-alpine
---
+++ 2.deploy-service.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -d "workspace" ]; then
+	mkdir workspace
+fi
+
+cd workspace
+
+sudo docker run -dt --name=gogs \
+ -p 10022:22 \
+ -p 3000:3000 \
+ --restart=always \
+ -v $PWD/gogs_data:/data \
+ gogs/gogs
+
+sudo docker run -d --name jenkins \
+ -p 8000:8080 \
+ -p 10000:10000 \
+ -p 50000:50000 \
+ -v $PWD/jenkins_data:/var/jenkins_home \
+ -v /usr/bin/docker:/usr/bin/docker \
+ -v /var/run/docker.sock:/var/run/docker.sock \
+ -v /usr/lib/x86_64-linux-gnu/libltdl.so.7:/usr/lib/x86_64-linux-gnu/libltdl.so.7 \
+ --restart=always \
+ --user root \
+ jenkins/jenkins:2.72
+
