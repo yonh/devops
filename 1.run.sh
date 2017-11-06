39ffabd1c943e73c816fae6e26e4a9850dc4eca3
yonh <azssjli@163.com>
2017-11-06 11:54:05 +0800

    update 1.sh, add install docker-compose

--- 1.run.sh
+++ 1.run.sh
@@ -8,6 +8,8 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
+sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 # sudo usermod -aG docker `whoami`
 
 
