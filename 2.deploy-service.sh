874d8cb009c60e594d09e096b193f3095a1a9ba8
yonh <azssjli@163.com>
2017-11-27 23:33:33 +0800

    add mysql sonarqube container

--- 2.deploy-service.sh
+++ 2.deploy-service.sh
@@ -25,3 +25,24 @@ sudo docker run -d --name jenkins \
  --user root \
  jenkins/jenkins:2.72
 
+sudo docker run -d --name mysql \
+ -p 3306:3306 \
+ -e MYSQL_USER=sonar \
+ -e MYSQL_PASSWORD=sonar \
+ -e MYSQL_DATABASE=sonar \
+ -e MYSQL_RANDOM_ROOT_PASSWORD=true \
+ --restart=always \
+ -v /tmp/mysql_data:/var/lib/mysql \
+ mysql/mysql-server:5.7
+
+MYSQL_IP="0.0.0.0"
+
+sudo docker run -d --name sonarqube \
+ -p 9000:9000 \
+ -p 9092:9092 \
+ -e SONARQUBE_JDBC_USERNAME=sonar \
+ -e SONARQUBE_JDBC_PASSWORD=sonar \
+ -e SONARQUBE_JDBC_URL="jdbc:mysql://${MYSQL_IP}:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" \
+ --restart=always \
+ sonarqube:6.4-alpine
+# default password admin / admin
